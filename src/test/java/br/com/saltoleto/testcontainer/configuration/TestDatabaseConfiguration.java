228544b11ae8e1555be8c726630dfe6e6ebb2c0e
Saltoleto <fernandosaltoleto@hotmail.com>
2023-09-13 09:08:58 -0300

    Update TestDatabaseConfiguration.java

--- src/test/java/br/com/saltoleto/testcontainer/configuration/TestDatabaseConfiguration.java
+++ src/test/java/br/com/saltoleto/testcontainer/configuration/TestDatabaseConfiguration.java
@@ -22,4 +22,33 @@ public class TestDatabaseConfiguration {
 
         return initializer;
     }
+
+     @Container
+    public static GenericContainer<?> sqsContainer = new GenericContainer<>("amazon/localstack")
+            .withExposedPorts(4566)
+            .withEnv("DEFAULT_REGION", "us-east-1")
+            .withEnv("AWS_ACCESS_KEY_ID", "test")
+            .withEnv("AWS_SECRET_ACCESS_KEY", "test");
+
+    private SqsAsyncClient sqsClient;
+
+    @BeforeEach
+    void setup() {
+        String sqsEndpoint = "http://" + sqsContainer.getHost() + ":" + sqsContainer.getMappedPort(4566);
+        sqsClient = SqsAsyncClient.builder()
+                .endpointOverride(URI.create(sqsEndpoint))
+                .build();
+    }
+
+    @AfterEach
+    void cleanup() {
+        sqsClient.close();
+    }
+
+    @Test
+    void testSqsIntegration() {
+        // Your SQS integration test logic here
+    }
+}
+In this example, we're using the GenericContainer from Testcontainers to run a local instance of AWS LocalStack, which provides a local version of SQS. We configure the client to connect to this local SQ
 }
